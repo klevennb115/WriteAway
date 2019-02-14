211be315b70f09cf986782221c98eb211229e7d9
Matt Kleven <klevennb115@gmail.com>
2019-02-14 14:44:33 -0600

    sweetalerts installed still working on editing

--- src/components/TextEditor/TextEditorPrompts/CreativeWritingPrompt.js
+++ src/components/TextEditor/TextEditorPrompts/CreativeWritingPrompt.js
@@ -1,34 +1,34 @@
-// import React, { Component } from 'react';
-// import { connect } from 'react-redux';
-// import promptSaga from '../../../redux/sagas/promptSaga';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import promptSaga from '../../../redux/sagas/promptSaga';
 
-// class CreativeWritingPrompt extends Component {
-//     render(){
-//         return(
-//             <div>
-//                 <button>Hey There</button>
-//             </div>
-//         )
-//     }
-// }
-
-// const mapStoreToProps = state => ({
-//     prompt: state.prompt,
-// });
-// export default connect(mapStoreToProps)(CreativeWritingPrompt);
-
-import React, {Component} from 'react';
-import '../TextEditor.css';
-
-
-class CreativeWritingPrompt extends Component{
+class CreativeWritingPrompt extends Component {
     render(){
         return(
             <div>
-                <button className="prompt-button">Hey There</button>
+                <button>Hey There</button>
             </div>
         )
     }
 }
-export default CreativeWritingPrompt;
+
+const mapStoreToProps = state => ({
+    prompt: state.prompt,
+});
+export default connect(mapStoreToProps)(CreativeWritingPrompt);
+
+// import React, {Component} from 'react';
+// import '../TextEditor.css';
+
+
+// class CreativeWritingPrompt extends Component{
+//     render(){
+//         return(
+//             <div>
+//                 <button className="prompt-button">Hey There</button>
+//             </div>
+//         )
+//     }
+// }
+// export default CreativeWritingPrompt;
 

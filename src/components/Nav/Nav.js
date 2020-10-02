775cad2f52cde8a211abb891ec2f7a1b0aa1169b
Matthew Kleven <klevennb115dev@gmail.com>
2020-10-01 23:23:42 -0500

    Fixed Navbar

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,35 +1,73 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { connect } from 'react-redux';
-import LogOutButton from '../LogOutButton/LogOutButton';
-import './Nav.css';
+// import React from 'react';
+// import { Link } from 'react-router-dom';
+// import { connect } from 'react-redux';
+// import LogOutButton from '../LogOutButton/LogOutButton';
+// import './Nav.css';
+
+// const Nav = (props) => (
+// 	<div className="nav">
+// 		<div >
+// 			<Link to="/home">
+// 				<h2 className="nav-title">Write Away</h2>
+// 			</Link>
+// 			<div className="nav-right">
+// 				<Link className="nav-link" to="/home">
+// 					{props.user.id ? "Home" : "Login / Register"}
+// 				</Link>
+// 				{props.user.id && (
+// 					<>
+// 						<LogOutButton className="nav-link" />
+// 					</>
+// 				)}
+// 				<Link className="nav-link" to="/prompt-manager">
+// 					Prompt Manager
+// 				</Link>
+// 				<Link className="nav-link" to="/about">
+// 					About
+// 				</Link>
+// 			</div>
+// 		</div>
+// 	</div>
+// );
+
+// const mapStateToProps = state => ({
+//   user: state.user,
+// });
+
+// export default connect(mapStateToProps)(Nav);
+
+import React from "react";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import LogOutButton from "../LogOutButton/LogOutButton";
+import "./Nav.css";
 
 const Nav = (props) => (
-	<div className="nav">
+	<div className="navbar navbar-expand-md navcolor">
 		<Link to="/home">
 			<h2 className="nav-title">Write Away</h2>
 		</Link>
-		<div className="nav-right">
-			<Link className="nav-link" to="/home">
+		<div className="navbar-nav ml-auto">
+			<Link className="nav-link nav-item" to="/home">
 				{props.user.id ? "Home" : "Login / Register"}
 			</Link>
 			{props.user.id && (
 				<>
-					<LogOutButton className="nav-link" />
+					<LogOutButton className=" nav-item nav-link" />
 				</>
 			)}
-			<Link className="nav-link" to="/prompt-manager">
+			<Link className="nav-link nav-item" to="/prompt-manager">
 				Prompt Manager
 			</Link>
-			<Link className="nav-link" to="/about">
+			<Link className="nav-link nav-item" to="/about">
 				About
 			</Link>
 		</div>
 	</div>
 );
 
-const mapStateToProps = state => ({
-  user: state.user,
+const mapStateToProps = (state) => ({
+	user: state.user,
 });
 
 export default connect(mapStateToProps)(Nav);
